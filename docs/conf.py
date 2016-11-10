e6458ccc489d6c2ffa4d184fbe98f6ec8042360b
Danny Hermes <daniel.j.hermes@gmail.com>
2016-11-09 17:27:27 -0800

    Slight tweaks to docs conf.

--- docs/conf.py
+++ docs/conf.py
@@ -368,6 +368,11 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'https://docs.python.org/2/': None,
-    'https://docs.scipy.org/doc/numpy/': None,
+    'python': ('https://docs.python.org/3.5', None),
+    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
 }
+
+# Autodoc config
+autoclass_content = 'both'
+autodoc_member_order = 'bysource'
+autodoc_mock_imports = []
