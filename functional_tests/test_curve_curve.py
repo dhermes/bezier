6c9951c7f278a402e3442f192ac0f74ec792085e
Danny Hermes <daniel.j.hermes@gmail.com>
2016-11-21 17:25:42 -0800

    Adding another curve-curve functional test.

--- functional_tests/test_curve_curve.py
+++ functional_tests/test_curve_curve.py
@@ -18,15 +18,32 @@ from bezier import _intersection_helpers
 
 
 PACK_DOUBLE = struct.Struct('>d').pack
+# g1 = sympy.Matrix([[s, 2 * s * (1 - s)]])
 CURVE1 = bezier.Curve(np.array([
     [0.0, 0.0],
     [0.5, 1.0],
     [1.0, 0.0],
 ]))
+# g2 = sympy.Matrix([[(9 - 8 * s) / 8, (2 * s - 1)**2 / 2]])
 CURVE2 = bezier.Curve(np.array([
     [1.125, 0.5],
     [0.625, -0.5],
     [0.125, 0.5],
+]))
+# g3 = sympy.Matrix([[3 * s, 6 * s * (1 - s)]])
+CURVE3 = bezier.Curve(np.array([
+    [0.0, 0.0],
+    [1.5, 3.0],
+    [3.0, 0.0],
+]))
+# g4 = sympy.Matrix([[
+#     -3 * (4 * s**2 + s - 4) / 4,
+#     (92 * s**2 - 77 * s + 24) / 16,
+# ]])
+CURVE4 = bezier.Curve(np.array([
+    [3.0, 1.5],
+    [2.625, -0.90625],
+    [-0.75, 2.4375],
 ]))
 
 
@@ -78,6 +95,14 @@ def curve_curve_check(curve1, curve2, s_vals, t_vals, points):
         assert_close(intersection.point[0], point[0])
         assert_close(intersection.point[1], point[1])
 
+        point_on1 = curve1.evaluate(s_val)
+        assert_close(point_on1[0], point[0])
+        assert_close(point_on1[1], point[1])
+
+        point_on2 = curve2.evaluate(t_val)
+        assert_close(point_on2[0], point[0])
+        assert_close(point_on2[1], point[1])
+
 
 def test_curves1_and_2():
     sq31 = np.sqrt(31.0)
@@ -91,3 +116,21 @@ def test_curves1_and_2():
         [s_val1, (16.0 - sq31) / 64.0],
     ])
     curve_curve_check(CURVE1, CURVE2, s_vals, t_vals, points)
+
+
+def test_curves3_and_4():
+    # NOTE: This clearly indicates there is a problem with
+    #       duplicates of intersections.
+    s_vals = np.array([0.25, 0.25, 0.25, 0.25, 0.875, 0.875, 0.875, 0.875])
+    t_vals = np.array([0.75, 0.75, 0.75, 0.75, 0.25, 0.25, 0.25, 0.25])
+    points = np.array([
+        [0.75, 1.125],
+        [0.75, 1.125],
+        [0.75, 1.125],
+        [0.75, 1.125],
+        [2.625, 0.65625],
+        [2.625, 0.65625],
+        [2.625, 0.65625],
+        [2.625, 0.65625],
+    ])
+    curve_curve_check(CURVE3, CURVE4, s_vals, t_vals, points)
