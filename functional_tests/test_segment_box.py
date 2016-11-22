3d7269852d001bd408eab9e4f83d0103fa94d647
Danny Hermes <daniel.j.hermes@gmail.com>
2016-11-22 14:42:21 -0800

    Adding plot utility to test_segment_box.

--- functional_tests/test_segment_box.py
+++ functional_tests/test_segment_box.py
@@ -8,6 +8,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
@@ -22,6 +23,21 @@ UNIT_SQUARE = np.array([
 ])
 
 
+class Config(object):
+    """Run-time configuration.
+
+    This is a mutable stand-in to allow test set-up to modify
+    global state.
+    """
+    AS_SCRIPT = False
+    MARKED = []
+
+    @classmethod
+    def mark(cls, func):
+        cls.MARKED.append(func)
+        return func
+
+
 def run_it(segment, expected=None):
     if expected is None:
         expected = _intersection_helpers.BoxIntersectionType.intersection
@@ -30,7 +46,26 @@ def run_it(segment, expected=None):
         UNIT_SQUARE, segment[[0], :], segment[[1], :])
     assert result is expected
 
+    if not Config.AS_SCRIPT:
+        return
 
+    line, = plt.plot([0, 1], [0, 1], alpha=0.0)
+    plt.fill_between([0, 1], [0, 0], [1, 1],
+                     alpha=0.5, color=line.get_color())
+    line, = plt.plot(segment[:, 0], segment[:, 1])
+    plt.plot(segment[0, 0], segment[0, 1], marker='o',
+             linestyle='None', color=line.get_color())
+    left_, bottom_ = np.min(segment, axis=0)
+    right_, top_ = np.max(segment, axis=0)
+    plt.fill_between([left_, right_], [bottom_, bottom_], [top_, top_],
+                     alpha=0.5, color=line.get_color())
+    plt.axis('scaled')
+    plt.xlim(-1.125, 2.125)
+    plt.ylim(-1.125, 2.125)
+    plt.show()
+
+
+@Config.mark
 def test_outside():
     segment_bottom_left = np.array([
         [0.25, -0.75],
@@ -60,6 +95,7 @@ def test_outside():
         run_it(segment, expected)
 
 
+@Config.mark
 def test_unsupported():
     segment = np.array([
         [-0.5, 0.5],
@@ -69,6 +105,7 @@ def test_unsupported():
         run_it(segment)
 
 
+@Config.mark
 def test_start_in_box():
     segments = (
         np.array([
@@ -121,6 +158,7 @@ def test_start_in_box():
         run_it(segment)
 
 
+@Config.mark
 def test_end_in_box():
     segments = (
         np.array([
@@ -169,6 +207,7 @@ def test_end_in_box():
         run_it(segment)
 
 
+@Config.mark
 def test_goes_through_box():
     segments = (
         np.array([
@@ -195,3 +234,13 @@ def test_goes_through_box():
 
     for segment in segments:
         run_it(segment)
+
+
+def main():
+    Config.AS_SCRIPT = True
+    for func in Config.MARKED:
+        func()
+
+
+if __name__ == '__main__':
+    main()
