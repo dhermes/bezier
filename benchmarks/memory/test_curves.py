85f0e05fd831a677e2cbcb92ac1f6b8fdd4ed3ff
Danny Hermes <daniel.j.hermes@gmail.com>
2017-08-14 20:50:47 -0700

    Increasing CircleCI memory interval to include 29.5312KB.
    See most recent failure:
    https://circleci.com/gh/dhermes/bezier/620

--- benchmarks/memory/test_curves.py
+++ benchmarks/memory/test_curves.py
@@ -30,7 +30,7 @@ def get_bounds():
     # NOTE: These bounds assume **just** the interpeter is running this code.
     #       When using a test runner like `py.test`, usage goes up by 4-8 KB.
     if os.getenv('CIRCLECI') == 'true':
-        return 22, 26
+        return 22, 30
     else:
         return 28, 32
 
