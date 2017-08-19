eadeeda1f15ea194736111d1336db7ce31d08380
Danny Hermes <daniel.j.hermes@gmail.com>
2017-08-18 17:11:44 -0700

    Changing time benchmark for CircleCI.
    Performance characteristics seem to have change with
    the 1.0 -> 2.0 change.
    
    - https://circleci.com/gh/dhermes/bezier/635
    - https://circleci.com/gh/dhermes/bezier/636

--- benchmarks/time/test_curves.py
+++ benchmarks/time/test_curves.py
@@ -22,7 +22,7 @@ FAILURES = (11, 20, 24, 42)
 
 def get_bounds():
     if os.getenv('CIRCLECI') == 'true':
-        return 85.0 / 1024.0, 95.0 / 1024.0
+        return 42.0 / 1024.0, 52.0 / 1024.0
     else:
         return 35.0 / 1024.0, 42.0 / 1024.0
 
