e9de1d533d6ceb6ca3f1661d77144abfd8403e1c
Danny Hermes <daniel.j.hermes@gmail.com>
2017-08-18 18:27:21 -0700

    Remove memory benchmark "special" bounds for CircleCI.
    The builds that have run so far seem to be in the observed
    range on my **own** install of Ubuntu 16.04:
    
    - 29.4688KB (https://circleci.com/gh/dhermes/bezier/635)
    - 29.5508KB (https://circleci.com/gh/dhermes/bezier/636)
    - 31.375 KB (https://circleci.com/gh/dhermes/bezier/638)

--- benchmarks/memory/test_curves.py
+++ benchmarks/memory/test_curves.py
@@ -29,10 +29,7 @@ SUCCESS_TEMPLATE = 'Memory usage: {:g}KB.'
 def get_bounds():
     # NOTE: These bounds assume **just** the interpeter is running this code.
     #       When using a test runner like `py.test`, usage goes up by 4-8 KB.
-    if os.getenv('CIRCLECI') == 'true':
-        return 22, 30
-    else:
-        return 28, 32
+    return 28, 32
 
 
 def intersect_all():
