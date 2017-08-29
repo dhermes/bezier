446c5a93aed7644064f83559212dc5d210e3f856
Danny Hermes <daniel.j.hermes@gmail.com>
2017-08-28 17:06:54 -0700

    Adding C-only header files for Fortran modules.
    I have plans to
    
    - Make them usable for C **and** C++
    - Remove `types.h` and `types.f90` (because the `c_double`
      intrinsic will do the same job)

---
+++ src/bezier/include/bezier_curve_intersection.h
@@ -0,0 +1,45 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdbool.h>
+
+enum BoxIntersectionType {
+  INTERSECTION = 0,
+  TANGENT = 1,
+  DISJOINT = 2,
+};
+
+void linearization_error(
+    double *nodes, int *degree, int *dimension, double *error);
+void segment_intersection(
+    double *start0, double *end0, double *start1, double *end1,
+    double *s, double *t, bool *success);
+void newton_refine_intersect(
+    double *s, double *nodes1, int *degree1,
+    double *t, double *nodes2, int *degree2,
+    double *new_s, double *new_t);
+void bbox_intersect(
+    int *num_nodes1, double *nodes1,
+    int *num_nodes2, double *nodes2, int *enum_);
+void parallel_different(
+    double *start0, double *end0,
+    double *start1, double *end1, bool *result);
+void from_linearized(
+    double *error1, double *start1, double *end1,
+    double *start_node1, double *end_node1, double *nodes1, int *degree1,
+    double *error2, double *start2, double *end2,
+    double *start_node2, double *end_node2, double *nodes2, int *degree2,
+    double *refined_s, double *refined_t,
+    bool *does_intersect, int *py_exc);
+void bbox_line_intersect(
+    int *num_nodes, double *nodes, double *line_start, double *line_end,
+    int *enum_)
---
+++ src/bezier/include/bezier_helpers.h
@@ -0,0 +1,19 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdbool.h>
+
+void cross_product(double *vec0, double *vec1, double *result);
+void bbox(
+    int *num_nodes, double *nodes, double *left,
+    double *right, double *bottom, double *top);
+void wiggle_interval(double *value, double *result, bool *success);
