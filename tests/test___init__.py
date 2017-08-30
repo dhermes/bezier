4cf6c6762fa0427cfd9233ed8b428210ca515cf1
Danny Hermes <daniel.j.hermes@gmail.com>
2017-08-30 14:16:12 -0700

    Fix for `Test_get_(include|lib)` when installed from eggs.
    Also
    
    - Lint fix an 80 char line
    - Using `pip install .` on AppVeyor instead of `python setup.py install`
      so that the package is installed into site-packages rather than
      via an egg

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -29,10 +29,7 @@ class Test_get_include(unittest.TestCase):
 
     def test_it(self):
         include_directory = self._call_function_under_test()
-        suffix = _suffix('include')
-        msg = 'Expected suffix: {}\nFull path: {}'.format(
-            suffix, include_directory)
-        self.assertTrue(include_directory.endswith(suffix), msg=msg)
+        _check_pkg_filename(self, include_directory, 'include')
 
 
 class Test_get_lib(unittest.TestCase):
@@ -44,14 +41,26 @@ class Test_get_lib(unittest.TestCase):
         return bezier.get_lib()
 
     @unittest.skipIf(
-        PLATFORM_SYSTEM == 'windows', 'Static library not yet built on Windows')
+        PLATFORM_SYSTEM == 'windows',
+        'Static library not yet built on Windows')
     def test_it(self):
         lib_directory = self._call_function_under_test()
-        suffix = _suffix('lib')
-        self.assertTrue(lib_directory.endswith(suffix))
+        _check_pkg_filename(self, lib_directory, 'lib')
 
 
-def _suffix(final_directory):
+def _full_suffix(last_segment):
     python_version = 'python{}.{}'.format(*sys.version_info[:2])
     return os.path.join(
-        'lib', python_version, 'site-packages', 'bezier', final_directory)
+        'lib', python_version, 'site-packages', 'bezier', last_segment)
+
+
+def _from_egg(path, last_segment):
+    suffix = os.path.join('bezier', last_segment)
+    return path.endswith(suffix) and '.egg' in path
+
+
+def _check_pkg_filename(test_case, path, last_segment):
+    suffix = _full_suffix(last_segment)
+    site_pkg = path.endswith(suffix)
+    from_egg = _from_egg(path, last_segment)
+    test_case.assertTrue(site_pkg or from_egg)
