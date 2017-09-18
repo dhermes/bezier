4186a52a748beb592d50851278049f1f28153e25
Danny Hermes <daniel.j.hermes@gmail.com>
2017-09-18 13:36:11 -0700

    Making sure to use `nox-automation >= 0.18.0`.
    This incorporated my hack to work around `__PYVENV_LAUNCHER__`.

--- nox.py
+++ nox.py
@@ -14,11 +14,9 @@ from __future__ import print_function
 
 import glob
 import os
-import sys
 import tempfile
 
 import nox
-import nox.command
 
 
 NUMPY = 'numpy'
@@ -43,33 +41,6 @@ JOURNAL_PATHS = {
 }
 
 
-class InstallCommand(nox.command.InstallCommand):
-
-    def run(self, venv):
-        pyvenv_launcher = venv.env.pop('__PYVENV_LAUNCHER__', None)
-        if pyvenv_launcher is not None:
-            msg = 'Popped __PYVENV_LAUNCHER__={}'.format(pyvenv_launcher)
-            print(msg, file=sys.stderr)
-        return super(InstallCommand, self).run(venv)
-
-    __call__ = run
-
-
-def install(session, *args):
-    # NOTE: This does exactly the same thing as
-    #       `nox.sessions.SessionConfig.install` except it uses a custom
-    #       `InstallCommand`.
-    # See: https://github.com/jonparrott/nox/issues/44
-    if not session.virtualenv:
-        raise ValueError(
-            'A session without a virtualenv can not install dependencies.')
-    if not args:
-        raise ValueError('At least one argument required to install().')
-
-    command = InstallCommand(args)
-    session._commands.append(command)
-
-
 def get_path(*names):
     return os.path.join(NOX_DIR, *names)
 
@@ -81,8 +52,7 @@ def pypy_setup(local_deps, session):
         local_deps.remove(NUMPY)
         local_deps = tuple(local_deps)
         # Install from the pre-built wheel.
-        install(
-            session,
+        session.install(
             '--use-wheel',
             '--no-index',
             '--find-links',
@@ -105,7 +75,7 @@ def update_generated(session, check):
     session.interpreter = SINGLE_INTERP
 
     # Install all dependencies.
-    install(session, 'Cython')
+    session.install('Cython')
 
     if check:
         command = get_path('scripts', 'remove_cython_files.py')
@@ -141,9 +111,9 @@ def unit_tests(session, python_version):
         env = None
 
     # Install all test dependencies.
-    install(session, *local_deps)
+    session.install(*local_deps)
     # Install this package.
-    install(session, '.')
+    session.install('.')
 
     # Run py.test against the unit tests.
     run_args = ['py.test'] + session.posargs + [get_path('tests')]
@@ -160,9 +130,9 @@ def cover(session):
 
     # Install all test dependencies.
     local_deps = BASE_DEPS + ('scipy', 'pytest-cov', 'coverage')
-    install(session, *local_deps)
+    session.install(*local_deps)
     # Install this package.
-    install(session, '.')
+    session.install('.')
 
     # Run py.test with coverage against the unit tests.
     run_args = ['py.test', '--cov=bezier', '--cov=tests']
@@ -186,9 +156,9 @@ def functional(session, python_version):
         env = {}
 
     # Install all test dependencies.
-    install(session, *local_deps)
+    session.install(*local_deps)
     # Install this package.
-    install(session, '.')
+    session.install('.')
 
     # Run py.test against the functional tests.
     run_args = ['py.test'] + session.posargs + [get_path('functional_tests')]
@@ -201,9 +171,9 @@ def docs(session):
     session.interpreter = SINGLE_INTERP
 
     # Install all dependencies.
-    install(session, *DOCS_DEPS)
+    session.install(*DOCS_DEPS)
     # Install this package.
-    install(session, '.')
+    session.install('.')
 
     # Run the script for building docs.
     command = get_path('scripts', 'build_docs.sh')
@@ -227,9 +197,9 @@ def doctest(session):
     session.interpreter = SINGLE_INTERP
     # Install all dependencies.
     local_deps = DOCS_DEPS + (MOCK_DEP,)
-    install(session, *local_deps)
+    session.install(*local_deps)
     # Install this package.
-    install(session, '.')
+    session.install('.')
 
     # Run the script for building docs and running doctests.
     run_args = get_doctest_args(session)
@@ -242,9 +212,9 @@ def docs_images(session):
     # Install all dependencies.
     local_deps = DOCS_DEPS
     local_deps += ('matplotlib >= 2.0.0', MOCK_DEP, SEABORN_DEP, 'pytest')
-    install(session, *local_deps)
+    session.install(*local_deps)
     # Install this package.
-    install(session, '.')
+    session.install('.')
 
     # Use custom RC-file for matplotlib.
     env = {'MATPLOTLIBRC': 'docs'}
@@ -279,9 +249,9 @@ def lint(session):
         'Pygments',
         'pylint',
     )
-    install(session, *local_deps)
+    session.install(*local_deps)
     # Install this package.
-    install(session, '.')
+    session.install('.')
 
     # Run the script to check that the README and other docs are valid.
     check_path = get_path('scripts', 'check_doc_templates.py')
@@ -333,9 +303,9 @@ def benchmark(session, target):
         run_args = ['py.test'] + session.posargs + [test_fi]
 
     # Install all test dependencies.
-    install(session, *local_deps)
+    session.install(*local_deps)
     # Install this package.
-    install(session, '.')
+    session.install('.')
 
     session.run(*run_args, env=functional_env())
 
@@ -351,7 +321,7 @@ def check_journal(session, machine):
     os.close(filehandle)
 
     # Set the journal environment variable and install ``bezier``.
-    install(session, NUMPY)  # Install requirement(s).
+    session.install(NUMPY)  # Install requirement(s).
     env = {'BEZIER_JOURNAL': journal_filename}
     session.run('pip', 'install', '.', env=env)
 
--- scripts/docker/bezier.Dockerfile
+++ scripts/docker/bezier.Dockerfile
@@ -3,10 +3,10 @@ FROM dhermes/python-multi
 # Install the current versions of nox and NumPy.
 RUN pip install --no-cache-dir \
   colorlog==2.10.0 \
-  nox-automation==0.17.0 \
+  nox-automation==0.18.0 \
   numpy==1.13.1 \
   py==1.4.34 \
-  six==1.10.0 \
+  six==1.11.0 \
   virtualenv==15.1.0
 
 # Build a NumPy wheel for PyPy since it takes a bit of time
