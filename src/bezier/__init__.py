3a77645b24de26628629a4eaf095eb2c502c2645
Danny Hermes <daniel.j.hermes@gmail.com>
2018-01-25 14:15:38 -0800

    Preparing for `0.6.3` release.

--- src/bezier/__init__.py
+++ src/bezier/__init__.py
@@ -45,7 +45,7 @@ except ImportError as exc:  # pragma: NO COVER
 #       and related. This is **entirely** to accomodate builds where
 #       ``bezier`` is imported from source (and not installed).
 __author__ = 'Danny Hermes'
-__version__ = '0.6.3.dev1'
+__version__ = '0.6.3'
 """str: The current version of :mod:`bezier`."""
 __all__ = [
     '__author__',
