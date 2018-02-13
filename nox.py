de5a14d166afa7bcd52da3e03868e9f8338f34fc
Danny Hermes <daniel.j.hermes@gmail.com>
2018-02-13 12:20:44 -0800

    Lint fix: Increasing max number of lines in Python modules.
    See failure: https://circleci.com/gh/dhermes/bezier/1221

--- nox.py
+++ nox.py
@@ -280,7 +280,7 @@ def lint(session):
     # Run Pylint over the library source.
     session.run(
         'pylint', '--rcfile', 'pylintrc',
-        '--max-module-lines=2369',
+        '--max-module-lines=2399',
         get_path('src', 'bezier'),
     )
     # Run Pylint over the tests source.
