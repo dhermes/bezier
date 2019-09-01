9b32c50db747e341b27d77b43745895f050e51dc
Danny Hermes <daniel.j.hermes@gmail.com>
2019-08-31 21:27:54 -0700

    Autoformatting all `.f90` files.
    Done via
    
      emacs --batch {filename}.f90 -f mark-whole-buffer -f f90-indent-subprogram -f save-buffer
    
    H/T to https://www.fortran90.org/src/faq.html

--- src/bezier/curve.f90
+++ src/bezier/curve.f90
@@ -818,13 +818,13 @@ contains
     !       derivative every time it is evaluated.
     first_deriv = (num_nodes - 1) * (nodes(:, 2:) - nodes(:, :num_nodes - 1))
     if (num_nodes == 0) then
-      error_val = -1
-      return
-   else if (num_nodes == 1) then
-      length = 0.0
-      error_val = 0
-      return
-   else if (num_nodes == 2) then
+       error_val = -1
+       return
+    else if (num_nodes == 1) then
+       length = 0.0
+       error_val = 0
+       return
+    else if (num_nodes == 2) then
        length = norm2(first_deriv)
        error_val = 0
        return
