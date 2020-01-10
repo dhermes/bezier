8c8c456f64e2934bcf758df8322cfc765cdeab54
Danny Hermes <daniel.j.hermes@gmail.com>
2020-01-10 08:01:33 -0800

    Referring to `BEZ_` prefixed ABI names in header files.
    (Still need to run `clang-format` on these, it is not yet installed
    on my laptop and I am currently offline.)

--- src/fortran/include/bezier/surface_intersection.h
+++ src/fortran/include/bezier/surface_intersection.h
@@ -31,16 +31,16 @@ typedef struct CurvedPolygonSegment {
     int edge_index;
 } CurvedPolygonSegment;
 
-void newton_refine_surface(int* num_nodes, double* nodes, int* degree,
+void BEZ_newton_refine_surface(int* num_nodes, double* nodes, int* degree,
     double* x_val, double* y_val, double* s, double* t, double* updated_s,
     double* updated_t);
-void locate_point_surface(int* num_nodes, double* nodes, int* degree,
+void BEZ_locate_point_surface(int* num_nodes, double* nodes, int* degree,
     double* x_val, double* y_val, double* s_val, double* t_val);
-void surface_intersections(int* num_nodes1, double* nodes1, int* degree1,
+void BEZ_surface_intersections(int* num_nodes1, double* nodes1, int* degree1,
     int* num_nodes2, double* nodes2, int* degree2, int* segment_ends_size,
     int* segment_ends, int* segments_size, CurvedPolygonSegment* segments,
     int* num_intersected, SurfaceContained* contained, Status* status);
-void free_surface_intersections_workspace(void);
+void BEZ_free_surface_intersections_workspace(void);
 
 #if defined(__cplusplus)
 }
