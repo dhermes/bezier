edf4d7b92c60fa51db7cdd99aaa2b422e0809762
Danny Hermes <daniel.j.hermes@gmail.com>
2020-01-18 14:06:02 -0800

    Adding `scripts/macos/test-venv/` to ignore / clean list.

--- noxfile.py
+++ noxfile.py
@@ -559,6 +559,7 @@ def clean(session):
         get_path("scripts", "macos", "__pycache__"),
         get_path("scripts", "macos", "dist_wheels"),
         get_path("scripts", "macos", "fixed_wheels"),
+        get_path("scripts", "macos", "test-venv"),
         get_path("src", "python", "bezier.egg-info"),
         get_path("src", "python", "bezier", "__pycache__"),
         get_path("src", "python", "bezier", "extra-dll"),
