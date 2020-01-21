876a3f94cf1f1a9fa93c4bc8811ac7e491b92718
Danny Hermes <daniel.j.hermes@gmail.com>
2020-01-20 21:26:24 -0800

    Readying for merge.

--- scripts/nox-install-for-doctest-linux.sh
+++ scripts/nox-install-for-doctest-linux.sh
@@ -24,31 +24,29 @@ SCRIPT_FI=$(readlink -f ${0})
 SCRIPTS_DIR=$(dirname ${SCRIPT_FI})
 REPO_ROOT=$(dirname ${SCRIPTS_DIR})
 LOCAL_WHEELHOUSE="${REPO_ROOT}/scripts/manylinux/fixed_wheels"
+DOCKER_IMAGE=quay.io/pypa/manylinux2010_x86_64
+DUMMY_IMAGE_NAME=bezier-manylinux
 # Variables within the container.
 PY_ROOT="/opt/python/cp38-cp38"
 BEZIER_ROOT="${REPO_ROOT}"
 WHEELHOUSE="${BEZIER_ROOT}/scripts/manylinux/fixed_wheels"
 
 # 0. Build the `manylinux` wheel (repaired with `auditwheel`).
-docker ps  # Diagnostic
-docker ps --no-trunc  # Diagnostic
-hostname  # Diagnostic
-env | grep -v COVERALLS_REPO_TOKEN # Diagnostic
 if [[ "${CI}" == "true" ]]; then
-    # Create a dummy container which will hold a volume with config.
-    docker create --volume "$(dirname "${BEZIER_ROOT}")" --name bezier-manylinux quay.io/pypa/manylinux2010_x86_64 /bin/true
+    # See: https://circleci.com/docs/2.0/building-docker-images/#mounting-folders
+    # Create a dummy container which will hold a volume.
+    docker create --volume "$(dirname "${BEZIER_ROOT}")" --name "${DUMMY_IMAGE_NAME}" "${DOCKER_IMAGE}" /bin/true
     # Copy source tree into this volume.
-    docker cp "${REPO_ROOT}" bezier-manylinux:"${BEZIER_ROOT}"
+    docker cp "${REPO_ROOT}" "${DUMMY_IMAGE_NAME}":"${BEZIER_ROOT}"
     # Rely on this dummy container.
-    VOLUME_ARG="--volumes-from=bezier-manylinux"
-    docker ps --all  # Diagnositc
+    VOLUME_ARG="--volumes-from=${DUMMY_IMAGE_NAME}"
 else
     CURRENT_CONTAINER_ID=$(docker ps --no-trunc --filter "id=$(hostname)" --format '{{ .ID }}')
     if [[ "$(echo "${CURRENT_CONTAINER_ID}" | wc -w)" == "1" ]]; then
         # Invoking `docker run` within a container.
         VOLUME_ARG="--volumes-from=${CURRENT_CONTAINER_ID}"
     else
-        # Running on host
+        # Running on host.
         VOLUME_ARG="--volume=${REPO_ROOT}:${BEZIER_ROOT}"
     fi
 fi
@@ -59,13 +57,12 @@ docker run \
     --env WHEELHOUSE="${WHEELHOUSE}" \
     --env BEZIER_ROOT="${BEZIER_ROOT}" \
     "${VOLUME_ARG}" \
-    quay.io/pypa/manylinux2010_x86_64 \
+    "${DOCKER_IMAGE}" \
     "${BEZIER_ROOT}/scripts/manylinux/build-wheel-for-doctest.sh"
 
 if [[ "${CI}" == "true" ]]; then
-    # Copy built wheels back into this container.
-    docker cp bezier-manylinux:"${LOCAL_WHEELHOUSE}" "${LOCAL_WHEELHOUSE}"
-    ls -alFG "${LOCAL_WHEELHOUSE}"  # Diagnostic
+    # Copy built wheel(s) back into this container.
+    docker cp "${DUMMY_IMAGE_NAME}":"${WHEELHOUSE}" "${LOCAL_WHEELHOUSE}"
 fi
 
 # 1. Install the `manylinux` wheel
