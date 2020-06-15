7ff12a00f472d8a5cf9306b5d1c4ed0c0f97c64e
Danny Hermes <daniel.j.hermes@gmail.com>
2020-06-14 21:55:00 -0700

    Updating to `Cython 0.29.20`.

--- noxfile.py
+++ noxfile.py
@@ -32,7 +32,7 @@ DEPS = {
     "cmake-format": "cmake-format >= 0.6.5",
     "cmake": "cmake >= 3.15.3",
     "coverage": "coverage",
-    "Cython": "Cython >= 0.29.19",
+    "Cython": "Cython >= 0.29.20",
     "docutils": "docutils",
     "flake8": "flake8",
     "flake8-import-order": "flake8-import-order",
