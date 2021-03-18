6d28a117c9b63197dc200704638b461cdc203c81
Danny Hermes <daniel.j.hermes@gmail.com>
2021-03-17 22:34:13 -0500

    Making sure imports appear at least once in `bezier.curved_polygon`.

--- src/python/bezier/curved_polygon.py
+++ src/python/bezier/curved_polygon.py
@@ -48,6 +48,7 @@ class CurvedPolygon:
     .. doctest:: curved-polygon-constructor
 
        >>> import bezier
+       >>> import numpy as np
        >>> nodes0 = np.asfortranarray([
        ...     [0.0,  1.0, 2.0],
        ...     [0.0, -1.0, 0.0],
